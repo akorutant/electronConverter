18e6331c0871ab485572be2a15a1da8b83df87c8
Saveliy Akoru <akorutant@mail.ru>
2023-02-25 21:42:38 +0300

    deleted files

--- src/renderer.js
+++ src/renderer.js
@@ -1,50 +1,37 @@
-const isNumeric = n => !isNaN(n);
+let currentCI = "length";
 
 window.addEventListener('DOMContentLoaded', () => {
-    let first = document.querySelector("#firstElement");
-    let answer = document.querySelector("#answer");
+    const resultLabel = document.querySelector("#label-result");
+    setCurrentCI(currentCI);
 
-    let radioButtons = document.querySelectorAll("input[type='radio']")
-    radioButtons.forEach((rbn) => {
-        rbn.addEventListener("click", (e) => {
-            let topSelect = document.querySelectorAll(".top-select")
-            let bottomSelect = document.querySelectorAll(".bottom-select")
-            topSelect.forEach((select) => {select.classList.remove("show")})
-            bottomSelect.forEach((bottom) => {bottom.classList.remove("show")})
-            if (e.target.value === 'volume') {
-                topSelect[2].classList.add("show");
-                bottomSelect[2].classList.add("show");
-            }
-            if (e.target.value === 'square') {
-                console.log(topSelect[2]);
-                topSelect[1].classList.add("show");
-                bottomSelect[1].classList.add("show");
-            }
-            if (e.target.value === 'length') {
-                topSelect[0].classList.add("show");
-                bottomSelect[0].classList.add("show");
-            }
+    const fromList = document.querySelector("#from");
+    fromList.addEventListener("change", (e) => {
+        const toList = document.querySelector("#to");
+        const ciLabel = document.querySelector("#label-ci");
 
-            if (e.target.value === "mass") {
-                topSelect[3].classList.add("show");
-                bottomSelect[3].classList.add("show");
-            }
-        })
+        ciLabel.innerText = e.target.value;
+        toList.innerHTML = "<option></option>";
+        Object.keys(valueList[currentCI][fromList.value]).forEach(e => toList.innerHTML += `<option>${e}</option>`);
     })
 
-    let button = document.querySelector("#button")
-    button.addEventListener("click", (e) => {
-        let top = document.querySelector(".top-select.show")
-        let bottom = document.querySelector(".bottom-select.show")
-        let values = getValueList(top.dataset.type);
-        
-        if (!isNumeric(first.value)) {
-            answer.textContent = "Указать можно только числа!";
-        }
+    const btnCalculate = document.querySelector("#btn-calculate");
+    btnCalculate.addEventListener("click", (e) => {
+        const fromValue = document.querySelector("#from").value;
+        const toValue = document.querySelector("#to").value;
+        const numValue = document.querySelector("#number").value;
 
-        else if (first.value) {
-            answer.textContent = Number(first.value) * values[top.value][bottom.value].multiple + values[top.value][bottom.value].CI;
-        }
- 
-    })
+        resultLabel.innerText = `= ${numValue * valueList[currentCI][fromValue][toValue]["multiple"]} ${toValue}`
+    });
 })
+
+function setCurrentCI(CI) {
+    const fromList = document.querySelector("#from");
+    const toList = document.querySelector("#to");
+
+    currentCI = CI;
+
+    toList.innerHTML = "<option></option>";
+
+    fromList.innerHTML = "<option></option>";
+    Object.keys(valueList[currentCI]).forEach(e => fromList.innerHTML += `<option>${e}</option>`);
+}
